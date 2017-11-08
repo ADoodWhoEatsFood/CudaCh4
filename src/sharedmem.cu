3f8710c5b7cd94f2b69f55c3354ec6a11884e5ac
Blaine Walls <blainer.walls@gmail.com>
2017-11-08 01:18:40 -0600

    Randomize number of elements in shared memory demo

--- src/sharedmem.cu
+++ src/sharedmem.cu
@@ -47,8 +47,7 @@ void demoDynamicShared()
 {
   std::vector<int> h_ints;
   std::srand(std::time(0));
-  //h_ints.resize(std::rand() % 1025); // Don't allocate more than we can bite off in one block
-  h_ints.resize(739); // Don't allocate more than we can bite off in one block
+  h_ints.resize(std::rand() % 1025); // Don't allocate more than we can bite off in one block
   std::cout << "Allocated " << h_ints.size() << " ints for dynamic shared memory demo.\n";
   for (int i{0}; i < h_ints.size(); ++i)
     h_ints[i] = i + 1;
